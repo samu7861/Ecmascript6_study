6d59697cf9bf12d2cef5b405b38ec317fbeb3abe
samu7861 <sfrancog7861@gmail.com>
2023-08-01 10:48:38 -0500

    strings n use of

---
+++ src/es6/02-strings.js
@@ -0,0 +1,17 @@
+let hello = "Hello"
+let world = "World"
+let epicPrhase = hello + " " +world
+
+
+//template literals
+let epicPrhase2 = `${hello} ${world}!`
+console.log(epicPrhase2)
+
+//multiline strings
+let lorem = "esto es un string \n " + "esta es otra linea"
+let lorem2 = `esta es una frase epica
+y esta es la continuación`
+
+console.log(lorem)
+
+console.log(lorem2)
