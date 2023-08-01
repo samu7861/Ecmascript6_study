9dc0ef84f771f5e8e58c07cb7ee28c07122b7c2f
samu7861 <sfrancog7861@gmail.com>
2023-07-31 21:20:24 -0500

    let n const, global properties

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,18 @@
+const animal = "iguana"
+animal = "xd"
+console.log(animal)
+
+let fruit = "Apple"
+fruit = "kiwi"
+console.log(fruit)
+
+const fruits = () => {
+    if(true){
+        var fruit1 = "apple"
+        let fruit2 = "kiwi"
+        const fruit3 = "banana"
+    }
+    console.log(fruit1)
+    console.log(fruit2)
+    console.log(fruit3)
+}
