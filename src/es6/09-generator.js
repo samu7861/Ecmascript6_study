ff1fe0857e1be673b78214e736cfede1db0a49e9
samu7861 <sfrancog7861@gmail.com>
2023-08-03 12:40:49 -0500

    generator yield exercises

--- src/es6/09-generator.js
+++ src/es6/09-generator.js
@@ -8,3 +8,13 @@ const it = iterate(["samui", "juan", "pedrito", "jen"])
 console.log(it.next().value)
 console.log(it.next().value)
 console.log(it.next().value)
+
+
+//ejercicio
+function* getId() {
+    // Tu código aquí 👈
+    let catID = 0
+    while (true) {
+      yield catID++
+    }
+  }
