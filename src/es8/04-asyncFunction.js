16aaa3dc5c918c4f7bb10d44973c83a9b2103280
samu7861 <sfrancog7861@gmail.com>
2023-08-08 16:38:51 -0500

    es8 async functions

---
+++ src/es8/04-asyncFunction.js
@@ -0,0 +1,13 @@
+const fnAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(()=>resolve("AsynC!!"), 2000)
+        : reject(new Error("Error!"))
+    })
+}
+
+const anotherFunction = async () => {
+    const something = await fnAsync()
+    console.log(something)
+    console.log("hello")
+}
