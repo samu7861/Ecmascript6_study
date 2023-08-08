081bcee708e6005338f4f47e9ee097f567f12ca5
samu7861 <sfrancog7861@gmail.com>
2023-08-08 16:18:55 -0500

    es8 objectValues

---
+++ src/es8/01-objectVaues.js
@@ -0,0 +1,10 @@
+const countries = {
+    MX: "mexico",
+    COL: "colombia",
+    CL: "chile",
+    PE: "peru"
+
+}
+
+//nos da cómo resultado un array sin la llave, solo el valor
+console.log(Object.values(countries))
