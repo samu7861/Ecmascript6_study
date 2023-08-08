d54655fa4be1769efbaa31d70a33fce3a0837892
samu7861 <sfrancog7861@gmail.com>
2023-08-08 16:26:40 -0500

    es8 trailling commas

---
+++ src/es8/02-StringPadding.js
@@ -0,0 +1,6 @@
+const string = "hello"
+
+
+//permite la modificación de strings
+console.log(string.padStart(6, "_"))
+console.log(string.padEnd(1, "_"))
---
+++ src/es8/03-trailling_commas.js
@@ -0,0 +1,9 @@
+const age = [10, 11, 18, 29]
+
+console.log(age)
+console.log(age.length)
+
+const age2 = [10, 11, 18, 29, , , , ]
+
+console.log(age2)
+console.log(age2.length)
