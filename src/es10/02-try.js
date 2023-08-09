cc4a795fc671d4406bc633282cf251937b3d21f2
samu7861 <sfrancog7861@gmail.com>
2023-08-09 08:35:42 -0500

    es10 try n from_entries

---
+++ src/es10/02-try.js
@@ -0,0 +1,15 @@
+try{
+    hello()
+}
+catch (error){
+    console.log(error)
+}
+
+//
+
+try {
+    anotherFunction()
+}
+catch {
+    console.log("esto es un error")
+}
---
+++ src/es10/03-from_entries.js
@@ -0,0 +1,3 @@
+const entries = new Map([["name", "samu"], ["age", 18]])
+console.log(Object.fromEntries(entries))
+console.log(entries)
