60e3c9c7a4c397e4b7ddd921a996b148ea3e6cdf
samu7861 <sfrancog7861@gmail.com>
2023-08-09 07:59:06 -0500

    async es9 2

--- src/es9/03-async.js
+++ src/es9/03-async.js
@@ -9,3 +9,12 @@ other.next().then(response => console.log(response.value))
 other.next().then(response => console.log(response.value))
 other.next().then(response => console.log(response.value))
 console.log("hello")
+
+async function arrayOfNames(array) {
+    for await (let value of array) {
+        console.log(value);
+    }
+}
+
+const names =arrayOfNames(["Alexa", "Oscar", "David"]);
+console.log("After");
